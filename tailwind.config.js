a32f98e9fe6f8d23eebb05733b8c5addf499adaa
asepsobarudin <asepsobarudin066@gmail.com>
2023-01-23 11:57:39 +0700

    first upload digital clock website

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Rampart: ["Rampart One", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
